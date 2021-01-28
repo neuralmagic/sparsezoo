2a5634e3c42402e118859cc63fec392951986fb0
Michael Goin <michael@neuralmagic.com>
2021-01-28 17:02:08 -0500

    Restrict ONNX<1.8.0 (#21)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 _deps = [
     "numpy>=1.0.0",
-    "onnx>=1.0.0",
+    "onnx>=1.0.0,<1.8.0",
     "pyyaml>=5.1.0",
     "requests>=2.0.0",
     "tqdm>=4.0.0",
