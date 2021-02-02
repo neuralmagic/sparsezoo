3567d06ac6689768954af32a06b41c47bae3b7bb
Kevin Escobar Rodriguez <kevin.rodriguez@neuralmagic.com>
2021-02-02 18:51:24 -0500

    style changes

--- tests/sparsezoo/models/classification/test_efficientnet.py
+++ tests/sparsezoo/models/classification/test_efficientnet.py
@@ -15,11 +15,7 @@
 import pytest
 
 from sparsezoo.models.classification import efficientnet_b0, efficientnet_b4
-from tests.sparsezoo.utils import (
-    ALL_MODELS_SKIP_MESSAGE,
-    SPARSEZOO_TEST_ALL_EFFICIENTNET,
-    model_constructor,
-)
+from tests.sparsezoo.utils import model_constructor
 
 
 @pytest.mark.parametrize(
@@ -55,9 +51,6 @@ def test_efficientnet_b0(
     )
 
 
-@pytest.mark.skipif(
-    not SPARSEZOO_TEST_ALL_EFFICIENTNET, reason="Not running full suite of tests"
-)
 @pytest.mark.parametrize(
     (
         "download,framework,repo,dataset,training_scheme,"
