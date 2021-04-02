a612b75127b2dbacb198cef48dfbf507cdfa1aa2
Mark Kurtz <mark@neuralmagic.com>
2021-04-02 11:54:22 -0400

    Abstract version to be used in setup and docs (#61)

--- src/sparsezoo/__init__.py
+++ src/sparsezoo/__init__.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__version__ = "0.2.0"
+
 from .main import *
 from .models.zoo import *
 from .objects import *
