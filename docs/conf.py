e7198b11f9a657bfbea47163a582ab6f4d974843
Mark Kurtz <mark@neuralmagic.com>
2021-04-02 01:11:32 -0400

    refactor for sphinx multiversion

--- docs/conf.py
+++ docs/conf.py
@@ -65,6 +65,24 @@ html_sidebars = {
     ],
 }
 
+# Whitelist pattern for tags (set to None to ignore all tags)
+smv_tag_whitelist = r'^.*$'
+
+# Whitelist pattern for branches (set to None to ignore all branches)
+smv_branch_whitelist = r'^.*$'
+
+# Whitelist pattern for remotes (set to None to use local branches only)
+smv_remote_whitelist = None
+
+# Pattern for released versions
+smv_released_pattern = r'^tags/.*$'
+
+# Format for versioned output directories inside the build directory
+smv_outputdir_format = '{ref.name}'
+
+# Determines whether remote or local git branches/tags are preferred if their output dirs conflict
+smv_prefer_remote_refs = False
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
