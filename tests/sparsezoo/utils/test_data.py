f7f385761e79943c7c62428d1c66d126c0106d1d
rahultuli <rahul@neuralmagic.com>
2021-07-19 14:39:30 -0400

    Update:Rename tests/utils.py to tests/helpers.py Fix:Unwrapping Single Input Errors Update:tests_data.py

--- tests/sparsezoo/utils/test_data.py
+++ tests/sparsezoo/utils/test_data.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 from typing import Iterable
 
+import numpy
 import numpy as np
 import pytest
 
@@ -88,11 +89,39 @@ def test_iter_batches_returns_iterable(both_datasets, batch_size, iterations):
         10,
     ],
 )
-def test_batch_is_in_list(both_datasets, batch_size, iterations):
-    for dataset in both_datasets:
-        loader = dataset.iter_batches(batch_size=batch_size, iterations=iterations)
-        for batch in loader:
-            assert isinstance(batch, list)
+def test_batch_is_in_list(multi_input_dataset, batch_size, iterations):
+    loader = multi_input_dataset.iter_batches(
+        batch_size=batch_size, iterations=iterations
+    )
+    for batch in loader:
+        assert isinstance(batch, list)
+
+
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        2,
+        10,
+    ],
+)
+@pytest.mark.parametrize(
+    "iterations",
+    [
+        1,
+        2,
+        4,
+        10,
+    ],
+)
+def test_batch_not_in_list_for_single_input(
+    single_input_dataset, batch_size, iterations
+):
+    loader = single_input_dataset.iter_batches(
+        batch_size=batch_size, iterations=iterations
+    )
+    for batch in loader:
+        assert not isinstance(batch, list) and isinstance(batch, numpy.ndarray)
 
 
 @pytest.mark.parametrize(
