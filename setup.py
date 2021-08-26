71921ba3fbc1376bb279f82e0966322c9ce610ed
rahultuli <rahul@neuralmagic.com>
2021-08-26 10:48:43 -0400

    Add:Support for ONNX versions 1.8.*, 1.9.*, upto 1.10.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ _PACKAGE_NAME = "sparsezoo" if is_release else "sparsezoo-nightly"
 
 _deps = [
     "numpy>=1.0.0",
-    "onnx>=1.0.0,<1.8.0",
+    "onnx>=1.0.0,<=1.10.1",
     "pyyaml>=5.1.0",
     "requests>=2.0.0",
     "tqdm>=4.0.0",
