6c6ef0a1e27f30a5b78dbf99990bc70c19422252
Kyle Sayers <kylesayrs@gmail.com>
2022-06-09 18:06:52 -0400

    Rotated chart axis

--- src/sparsezoo/analysis/utils/chart.py
+++ src/sparsezoo/analysis/utils/chart.py
@@ -20,13 +20,12 @@ from sparsezoo.analysis import ModelAnalysis
 
 def draw_param_sparsity_chart(model_analysis: ModelAnalysis, out_path: str):
     figure, param_axes = plt.subplots()
-    ops_axes = param_axes.twiny()
+    ops_axes = param_axes.twinx()
 
     text_size = 8
-    height = 0.3
+    width = 0.3
 
     parameterized_prunable_nodes = [node for node in model_analysis.layers if node.parameterized_and_prunable]
-    parameterized_prunable_nodes.reverse()
 
     node_data = {
         "node_names": [],
@@ -39,38 +38,43 @@ def draw_param_sparsity_chart(model_analysis: ModelAnalysis, out_path: str):
     }
     for node_i, node in enumerate(parameterized_prunable_nodes):
         node_data["node_names"].append(node.name)
-        node_data["parameters_pos"].append(node_i + height / 2)
-        node_data["ops_pos"].append(node_i - height / 2)
+        node_data["parameters_pos"].append(node_i - width / 2)
+        node_data["ops_pos"].append(node_i + width / 2)
         node_data["sparse_parameters"].append(node.num_sparse_parameters)
         node_data["dense_parameters"].append(node.num_parameters - node.num_sparse_parameters)
         node_data["sparse_ops"].append(node.num_sparse_ops)
         node_data["dense_ops"].append(node.num_dense_ops)
 
-    param_axes.barh(node_data["parameters_pos"], node_data["sparse_parameters"], height=height, color="deeppink", label="sparse parameters")
-    param_axes.barh(node_data["parameters_pos"], node_data["dense_parameters"], left=node_data["sparse_parameters"], height=height, color="blue", label="dense parameters")
+    param_axes.bar(node_data["parameters_pos"], node_data["sparse_parameters"], width=width, color="deeppink", label="sparse parameters")
+    param_axes.bar(node_data["parameters_pos"], node_data["dense_parameters"], bottom=node_data["sparse_parameters"], width=width, color="blue", label="dense parameters")
 
-    ops_axes.barh(node_data["ops_pos"], node_data["sparse_ops"], height=height, color="purple", label="sparse operations")
-    ops_axes.barh(node_data["ops_pos"], node_data["dense_ops"], left=node_data["sparse_ops"], height=height, color="darkblue", label="dense operations")
+    ops_axes.bar(node_data["ops_pos"], node_data["sparse_ops"], width=width, color="purple", label="sparse operations")
+    ops_axes.bar(node_data["ops_pos"], node_data["dense_ops"], bottom=node_data["sparse_ops"], width=width, color="darkblue", label="dense operations")
 
-    param_axes.set_yticks(numpy.arange(len(parameterized_prunable_nodes)))
-    param_axes.set_yticklabels(node_data["node_names"])
-    param_axes.invert_yaxis()
-    param_axes.set_xlabel('Number of parameters')
+    param_axes.set_xticks(numpy.arange(len(parameterized_prunable_nodes)))
+    param_axes.set_xticklabels(node_data["node_names"])
+    param_axes.invert_xaxis()
+    param_axes.set_ylabel('Number of parameters')
     param_axes.legend(loc="upper left")
 
-    ops_axes.set_yticks(numpy.arange(len(parameterized_prunable_nodes)))
-    ops_axes.invert_yaxis()
-    ops_axes.set_xlabel('Number of floating-point/ integer operations')
+    ops_axes.set_xticks(numpy.arange(len(parameterized_prunable_nodes)))
+    ops_axes.invert_xaxis()
+    ops_axes.set_ylabel('Number of floating-point/ integer operations')
     ops_axes.legend(loc="upper right")
 
     for node_i in range(len(parameterized_prunable_nodes)):
-        parameter_sparsity = node_data["sparse_parameters"][node_i] / (node_data["dense_parameters"][node_i] + node_data["sparse_parameters"][node_i])
-        operation_sparsity = node_data["sparse_ops"][node_i] / (node_data["dense_ops"][node_i] + node_data["sparse_ops"][node_i])
-        param_axes.text(node_data["sparse_parameters"][node_i] + node_data["dense_parameters"][node_i] + 50, node_data["parameters_pos"][node_i], f"{parameter_sparsity:.0%}", color='black', fontweight='bold', size=text_size, verticalalignment="center")
-        ops_axes.text(node_data["sparse_ops"][node_i] + node_data["dense_ops"][node_i] + 50, node_data["ops_pos"][node_i], f"{operation_sparsity:.0%}", color='black', fontweight='bold', size=text_size, verticalalignment="center")
+        total_parameters = node_data["dense_parameters"][node_i] + node_data["sparse_parameters"][node_i]
+        if total_parameters > 0:
+            parameter_sparsity = node_data["sparse_parameters"][node_i] / total_parameters
+            param_axes.text(node_data["parameters_pos"][node_i], total_parameters, f"{parameter_sparsity:.0%}", color='black', fontweight='bold', size=text_size, verticalalignment="bottom", horizontalalignment="center")
+
+        total_ops = node_data["dense_ops"][node_i] + node_data["sparse_ops"][node_i]
+        if total_ops > 0:
+            operation_sparsity = node_data["sparse_ops"][node_i] / total_ops
+            ops_axes.text(node_data["ops_pos"][node_i], total_ops, f"{operation_sparsity:.0%}", color='black', fontweight='bold', size=text_size, verticalalignment="bottom", horizontalalignment="center")
 
     param_axes.set_title('Model parameter and operation sparsity')
 
-    plt.setp(param_axes.get_yticklabels(), rotation=30)
+    plt.setp(param_axes.get_xticklabels(), rotation=30, ha="right")
 
     plt.show()
--- tests/sparsezoo/analysis/utils/test_chart.py
+++ tests/sparsezoo/analysis/utils/test_chart.py
@@ -7,8 +7,11 @@ from sparsezoo.analysis.utils.chart import (
 )
 
 if __name__ == "__main__":
-    zoo_model = Zoo.load_model_from_stub("zoo:cv/classification/resnet_v1-50"
-    "/pytorch/sparseml/imagenet/pruned85_quant-none-vnni")
+    #zoo_model = Zoo.load_model_from_stub("zoo:cv/classification/resnet_v1-50"
+    #"/pytorch/sparseml/imagenet/pruned85_quant-none-vnni")
+    zoo_model = Zoo.load_model_from_stub("zoo:nlp/question_answering/bert-base/"
+    "pytorch/huggingface/squad/"
+    "12layer_pruned80_quant-none-vnni")
     zoo_model.onnx_file.download()
     onnx_path = zoo_model.onnx_file.downloaded_path()
 
