69708d571e211515d6d5ca6411d0e6a457981ff3
George Ohashi <george@neuralmagic.com>
2022-08-09 16:37:37 -0400

    polished

--- tests/sparsezoo/git_server_model/test_validations.py
+++ tests/sparsezoo/git_server_model/test_validations.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from sparsezoo.git_server_model.validations import BenchmarkValidation, ModelValidation
+# flake8: noqa
+
+from sparsezoo.git_server.validations import BenchmarkValidation, ModelValidation
