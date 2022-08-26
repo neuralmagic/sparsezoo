f90b33f934a748bd23d0fb2d578cb704e318edfa
Corey Lowman <corey@neuralmagic.com>
2022-08-26 15:02:25 -0400

    Minifying tests

--- tests/sparsezoo/utils/test_authentication.py
+++ tests/sparsezoo/utils/test_authentication.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,6 +37,9 @@ def test_load_token_yaml_fail(tmp_path):
     assert _maybe_load_token(path) is None
 
 
+_OLD_DATE = (datetime.now() - timedelta(days=40)).timestamp()
+
+
 @pytest.mark.parametrize(
     "content",
     [
@@ -44,11 +47,7 @@ def test_load_token_yaml_fail(tmp_path):
         {CREDENTIALS_YAML_TOKEN_KEY: {}},
         {CREDENTIALS_YAML_TOKEN_KEY: {"token": "asdf"}},
         {CREDENTIALS_YAML_TOKEN_KEY: {"created": "asdf"}},
-        {
-            CREDENTIALS_YAML_TOKEN_KEY: {
-                "created": (datetime.now() - timedelta(days=40)).timestamp()
-            }
-        },
+        {CREDENTIALS_YAML_TOKEN_KEY: {"created": _OLD_DATE}},
     ],
 )
 def test_load_token_failure_cases(tmp_path, content):
