dbe66fdf45a5b09cc11176711c5d10180f66fbce
Rahul Tuli <rahul@neuralmagic.com>
2023-02-14 23:56:07 -0500

    Rename: `BenchmarkSetup` --> `BenchmarkScenario` (#260)

--- src/sparsezoo/analysis/analysis.py
+++ src/sparsezoo/analysis/analysis.py
@@ -60,7 +60,7 @@ from sparsezoo.utils import (
 __all__ = [
     "NodeInferenceResult",
     "ImposedSparsificationInfo",
-    "BenchmarkSetup",
+    "BenchmarkScenario",
     "BenchmarkResult",
     "NodeAnalysis",
     "ModelAnalysis",
@@ -131,7 +131,7 @@ class ImposedSparsificationInfo(YAMLSerializableBaseModel):
     )
 
 
-class BenchmarkSetup(YAMLSerializableBaseModel):
+class BenchmarkScenario(YAMLSerializableBaseModel):
     """
     Schema representing information for a benchmarking experiment
     """
@@ -190,7 +190,7 @@ class BenchmarkResult(YAMLSerializableBaseModel):
     Schema representing results from a benchmarking experiment
     """
 
-    setup: BenchmarkSetup = Field(
+    setup: BenchmarkScenario = Field(
         description="Information regarding hardware, cores, batch_size, scenario and "
         "other info needed to run benchmark"
     )
