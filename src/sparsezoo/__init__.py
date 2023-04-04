f6844b8d8b4ba3a0dafc4e2f2dc419f0b805c612
Mark Kurtz <mark.kurtz@neuralmagic.com>
2023-04-04 14:34:23 -0400

    Google Analtyics fixes (#289)
    * Google Analtyics fixes
    
    * fix style

--- src/sparsezoo/__init__.py
+++ src/sparsezoo/__init__.py
@@ -26,4 +26,4 @@ from . import deployment_package as deployment_package_module
 from .deployment_package import *
 from .analytics import sparsezoo_analytics as _analytics
 
-_analytics.send_event("python.init")
+_analytics.send_event("python__init")
