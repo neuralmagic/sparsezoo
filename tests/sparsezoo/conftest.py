54684f6c063c92c9f3f228eff675260fff68f0be
Damian <damian@neuralmagic.com>
2023-06-06 15:39:32 +0000

    fix conftest

--- tests/sparsezoo/conftest.py
+++ tests/sparsezoo/conftest.py
@@ -19,35 +19,36 @@ from typing import List
 import pytest
 
 
-def _get_file_count(directory: str) -> List[str]:
-    list = []
+def _get_files(directory: str) -> List[str]:
+    list_filepaths = []
     for root, dirs, files in os.walk(directory):
         for file in files:
-            list.append(os.path.join(os.path.abspath(root), file))
-    return list
+            list_filepaths.append(os.path.join(os.path.abspath(root), file))
+    return list_filepaths
 
 
 @pytest.fixture(scope="session", autouse=True)
 def check_for_created_files():
-    start_file_count_root = _get_file_count(directory=r".")
-    start_file_count_temp = _get_file_count(directory=tempfile.gettempdir())
+    start_files_root = _get_files(directory=r".")
+    start_files_temp = _get_files(directory=tempfile.gettempdir())
     yield
+    end_files_root = _get_files(directory=r".")
+    end_files_temp = _get_files(directory=tempfile.gettempdir())
 
-    end_file_count_root = _get_file_count(directory=r".")
-    end_file_count_temp = _get_file_count(directory=tempfile.gettempdir())
-
-    assert len(start_file_count_root) >= len(end_file_count_root), (
-        f"{len(end_file_count_root) - len(start_file_count_root)} "
+    assert len(start_files_root) >= len(end_files_root), (
+        f"{len(end_files_root) - len(start_files_root)} "
         f"files created in current working "
-        f"directory during pytest run."
+        f"directory during pytest run. "
+        f"Created files: {set(end_files_root) - set(start_files_root)}"
     )
-    max_allowed_temp_files = 5
-    print(set(end_file_count_temp) - set(start_file_count_temp))
-    assert len(start_file_count_temp) + max_allowed_temp_files >= len(
-        end_file_count_temp
-    ), (
-        f"{len(end_file_count_temp) - len(start_file_count_temp)} "
-        f"files created in /tmp "
-        f"directory during pytest run. Diff: "
-        f"{set(end_file_count_temp) - set(start_file_count_temp)}"
+    max_allowed_sized_temp_files_megabytes = 1
+    size_of_temp_files_bytes = sum(
+        os.path.getsize(path) for path in set(end_files_temp) - set(start_files_temp)
+    )
+    size_of_temp_files_megabytes = size_of_temp_files_bytes / 1024 / 1024
+
+    assert max_allowed_sized_temp_files_megabytes >= size_of_temp_files_megabytes, (
+        f"{size_of_temp_files_megabytes} "
+        f"megabytes of temp files created in temp directory during pytest run. "
+        f"Created files: {set(end_files_temp) - set(start_files_temp)}"
     )
