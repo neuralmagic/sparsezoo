91afe56e72fe9b7a4bfe47e8dc38039c4ca506cf
Rahul Tuli <rahul@neuralmagic.com>
2023-06-12 16:54:44 -0400

    Update numpy

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ print(f"loaded version {version} from src/sparsezoo/version.py")
 _PACKAGE_NAME = "sparsezoo" if is_release else "sparsezoo-nightly"
 
 _deps = [
-    "numpy>=1.0.0,<=1.21.6",
+    "numpy>=1.0.0",
     "onnx>=1.5.0,<1.15.0",
     "pyyaml>=5.1.0",
     "requests>=2.0.0",
