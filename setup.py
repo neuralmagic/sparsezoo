945616bf9e8cdf7f8a124267918d0cafb3a42522
Michael Goin <michael@neuralmagic.com>
2023-06-27 10:33:30 -0400

    Lift protobuf restriction to allow onnx 1.14 (#337)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ _deps = [
     "tqdm>=4.0.0",
     "pydantic>=1.8.2",
     "click>=7.1.2,!=8.0.0",  # latest version < 8.0 + blocked version with reported bug
-    "protobuf>=3.12.2,<4",
+    "protobuf>=3.12.2",
     "pandas>1.3",
     "py-machineid>=0.3.0",
     "geocoder>=1.38.0",
