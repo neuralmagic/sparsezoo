cb0b68b19d77ed1072affb2c77e67dba99702ae5
Dipika Sikka <ds3822@columbia.edu>
2023-07-17 10:27:53 -0400

    Fix the dimension for nodes with 0 dim (#344)
    * fix the dimension for nodes with 0 dim
    
    * move fix before check

--- src/sparsezoo/utils/onnx.py
+++ src/sparsezoo/utils/onnx.py
@@ -296,6 +296,9 @@ def group_four_block(array: numpy.ndarray, pad_value: bool = True) -> numpy.ndar
     :param pad_value: value to pad remainder block with
     :return: array grouped into blocks with shape [-1, 4]
     """
+    if array.ndim == 0:
+        array = numpy.array([array], dtype=array.dtype)
+
     # Transpose so input channels are last
     if array.ndim > 2:
         input_channel_dim = 1
