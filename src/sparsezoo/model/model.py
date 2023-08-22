24cfe839dff02422be113e4729dce6d9f84ed58b
Rahul Tuli <rahul@neuralmagic.com>
2023-08-22 13:27:27 -0400

    Also Extract model.onnx.tar.gz on Model download (#359)
    * Also Extract model.onnx.tar.gz on Model download
    
    * Style

--- src/sparsezoo/model/model.py
+++ src/sparsezoo/model/model.py
@@ -232,6 +232,21 @@ class Model(Directory):
         """
         return self._stub_params
 
+    @property
+    def path(self) -> str:
+        """
+        Download(if not already downloaded) and return the local path of the model
+
+        :return: the local path of the model
+        """
+        # download the model if not already downloaded
+        super().path
+
+        # extract onnx_model if it is a tar.gz
+        self.onnx_model.path
+
+        return self._path
+
     def generate_outputs(
         self, engine_type: str, save_to_tar: bool = False
     ) -> Generator[List[numpy.ndarray], None, None]:
@@ -294,6 +309,8 @@ class Model(Directory):
                             f"Attempted to download file {key}, "
                             f"but it is `None`. The file is being skipped..."
                         )
+            # extract the onnx model if needed
+            self.onnx_model.path
 
         return all(downloads)
 
