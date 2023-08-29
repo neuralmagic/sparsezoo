755140106a5aa074b5ec7d9010a81963d109494f
Benjamin <ben@neuralmagic.com>
2023-08-29 11:41:58 -0400

    fix utils/onnx init file

--- src/sparsezoo/utils/onnx/__init__.py
+++ src/sparsezoo/utils/onnx/__init__.py
@@ -11,3 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# flake8: noqa
+# isort: skip_file
+
+from .analysis import *
+from .external_data import *
