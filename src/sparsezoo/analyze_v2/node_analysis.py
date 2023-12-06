b442cba01be9f20de10bbd0387ef9c13748dfddc
George Ohashi <george@neuralmagic.com>
2023-12-05 22:25:08 -0500

    comments

--- src/sparsezoo/analyze_v2/node_analysis.py
+++ src/sparsezoo/analyze_v2/node_analysis.py
@@ -16,7 +16,7 @@ from typing import Optional
 
 import yaml
 
-from sparsezoo.analyze_v2.model_validator import NodeAnalysisModel
+from sparsezoo.analyze_v2.schemas import NodeAnalysisSchema
 
 from .memory_access_analysis import MemoryAccessAnalysis
 from .operation_analysis import OperationAnalysis
@@ -37,19 +37,8 @@ class NodeAnalysis:
             model_graph, node, node_shape
         )
 
-        self.params_counts, self.params_counts_sparse = 0, 0
-        self.params_bits, self.params_bits_quant = 0, 0
-
-        self.ops_counts, self.ops_counts_sparse = 0, 0
-        self.ops_bits, self.ops_bits_quant = 0, 0
-
-        self.mem_counts, self.mem_counts_sparse = 0, 0
-        self.mem_bits, self.mem_bits_quant = 0, 0
-
-        self._update_metadata()
-
     def to_dict(self):
-        return NodeAnalysisModel(
+        return NodeAnalysisSchema(
             name=self.node.name,
             op_type=self.node.op_type,
             graph_order=self.graph_order,
@@ -62,19 +51,3 @@ class NodeAnalysis:
 
     def to_yaml(self):
         return yaml.dump(self.to_dict())
-
-    def _update_metadata(self):
-        self.params_counts = self.parameter_analysis.counts["counts"]
-        self.params_counts_sparse = self.parameter_analysis.counts["counts_sparse"]
-        self.params_bits = self.parameter_analysis.bits["bits"]
-        self.params_bits_quant = self.parameter_analysis.bits["bits_quant"]
-
-        self.ops_counts = self.operation_analysis.counts["counts"]
-        self.ops_counts_sparse = self.operation_analysis.counts["counts_sparse"]
-        self.ops_bits = self.operation_analysis.bits["bits"]
-        self.ops_bits_quant = self.operation_analysis.bits["bits_quant"]
-
-        self.mem_counts = self.memory_access_analysis.counts["counts"]
-        self.mem_counts_sparse = self.memory_access_analysis.counts["counts_sparse"]
-        self.mem_bits = self.memory_access_analysis.bits["bits"]
-        self.mem_bits_quant = self.memory_access_analysis.bits["bits_quant"]
