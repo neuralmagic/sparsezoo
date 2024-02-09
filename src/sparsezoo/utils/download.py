b8f5ae26ee9f5fb2eecb9856c159c2f00401af6d
George Ohashi <george@neuralmagic.com>
2024-02-09 19:56:20 +0000

    doc string and return types

--- src/sparsezoo/utils/download.py
+++ src/sparsezoo/utils/download.py
@@ -32,6 +32,8 @@ __all__ = ["download_file"]
 
 _LOGGER = logging.getLogger(__name__)
 
+CHUNK_BYTES = 500_000_000  # 500 MB
+
 
 def download_file(
     url_path: str,
@@ -41,16 +43,11 @@ def download_file(
 ):
     """
     Download a file from the given url to the desired local path
+
     :param url_path: the source url to download the file from
     :param dest_path: the local file path to save the downloaded file to
-    :param overwrite: True to overwrite any previous files if they exist,
-        False to not overwrite and raise an error if a file exists
     :param num_retries: number of times to retry the download if it fails
-    :param show_progress: True to show a progress bar for the download,
-        False otherwise
-    :param progress_title: The title to show with the progress bar
-    :raise PreviouslyDownloadedError: raised if file already exists at dest_path
-        nad overwrite is False
+
     """
     downloader = Downloader(
         url=url_path, download_path=dest_path, max_retries=num_retries
@@ -70,6 +67,8 @@ class Job:
 
 
 class JobQueue(Queue):
+    """Add description to display the status in tqdm progress bar"""
+
     def __init__(self, maxsize=0, description: str = ""):
         super().__init__(maxsize)
         self.description = description
@@ -81,18 +80,19 @@ class Downloader:
         url: str,
         download_path: str,
         max_retries: int = 3,
-        chunk_bytes: int = 500_000_000,
+        chunk_bytes: int = CHUNK_BYTES,
     ):
 
         self.url = url
         self.download_path = download_path
         self.max_retries = max_retries
-        self.file_size = self.get_file_size()
+        self.file_size: int = self.get_file_size()
         self.chunk_bytes = chunk_bytes
         self.job_queues = Queue()
         self._lock = threading.Lock()
 
-    def is_range_header_supported(self):
+    def is_range_header_supported(self) -> bool:
+        """Check if chuck download is supported"""
         response = requests.head(
             self.url, headers={"Accept-Encoding": "identity"}, allow_redirects=True
         )
@@ -101,6 +101,7 @@ class Downloader:
         return False
 
     def get_file_size(self):
+        """Get file size of the requested file"""
         response = requests.head(
             self.url, headers={"Accept-Encoding": "identity"}, allow_redirects=True
         )
@@ -109,8 +110,42 @@ class Downloader:
             return file_size
         raise ValueError(f"Invalid download URL: {self.url}")
 
-    def queue_chunk_download_jobs(self):
+    def queue_chunk_download_jobs(self) -> None:
+        """
+        Queues two jobs: file chunk download and combining the chunks.
 
+        This method calculates the number of download jobs needed based on the size of
+         the file to be downloadedand the size of each chunk. It creates download jobs
+         for each chunk, where each job is responsible for downloading a specific byte
+         range of the file. After queuing all the chunk download jobs, it queues an
+         additional job to combine these chunks into the final file.
+
+        Each download job is added to a JobQueue with a descrition (shown in the
+         progress bar), and the jobs areconfigured with the necessary parameters,
+         including the path where the chunk will be saved andthe 'Range' header to
+         download the specified byte range. The method ensures that the byte ranges
+         are calculated correctly to cover the entire file, even if the file size is
+         not a multiple of the chunk size.
+
+        After all chunk download jobs are queued, if at least one chunk download job
+         exists, a final job is queued to combine all downloaded chunks into the final
+         file and deletethe individual chunk files. This combining job is added to a
+         separate JobQueue with its own descriptive label.
+
+        Side effects:
+            - Queues jobs in the instance's `job_queues` attribute, which should be a
+               Queue of JobQueues.
+            - Utilizes the `download_file` method to download each chunk.
+            - Utilizes the `get_chunk_file_path` method to determine the path for each
+               chunk's temporary file.
+            - Utilizes the `combine_chunks_and_delete` method as the final step to
+               combine all chunks and clean up.
+
+        Note:
+            This method does not start the download jobs; it only queues them.
+            The jobs need to be executed by a worker or scheduler that processes the
+             queued JobQueues.
+        """
         download_jobs: Queue[Job] = JobQueue(description="Downloading Chunks")
         num_download_jobs = self.file_size // self.chunk_bytes + int(
             self.chunk_bytes % (self.file_size - 1) != 0
@@ -145,24 +180,43 @@ class Downloader:
             )
 
         # add chunk combine job at the end
-        chunk_combine_job = JobQueue(description="Combining Chunks")
-        chunk_combine_job.put(
-            Job(
-                id=download_jobs.qsize() + 1,
-                func=self.combine_chunks_and_delete,
-                func_kwargs={
-                    "download_path": self.download_path,
-                },
+        if download_jobs.qsize() > 0:
+            chunk_combine_job = JobQueue(description="Combining Chunks")
+            chunk_combine_job.put(
+                Job(
+                    id=download_jobs.qsize() + 1,
+                    func=self.combine_chunks_and_delete,
+                    func_kwargs={
+                        "download_path": self.download_path,
+                    },
+                )
             )
-        )
-        self.job_queues.put(download_jobs)
-        self.job_queues.put(chunk_combine_job)
+            self.job_queues.put(download_jobs)
+            self.job_queues.put(chunk_combine_job)
 
-    def download(self, *args, **kwargs):
+    def download(self, *args, **kwargs) -> None:
         self.queue_jobs(*args, **kwargs)
         self.run()
 
-    def queue_jobs(self):
+    def queue_jobs(self) -> None:
+        """
+        Determines the download strategy and queues the appropriate jobs.
+
+        This method decides whether to use chunked download or a regular download based
+         on the server's support forrange requests. If the server supports range
+         requests,it queues chunk download jobs for different portions of the file,
+         allowing paralleldownloads of file chunks. Otherwise, it falls back to a
+         regulardownload,queuing a single job to download the entire file at once.
+
+        For chunked downloads, it calls `queue_chunk_download_jobs` to calculate
+         the number of chunks and queue separate download jobs for each chunk. It
+         then queues a finaljob to combine these chunks into the final file.
+
+        For a regular download, it queues a single job with the `download_file` method
+         as thetarget, passing the download path as an argument. This job is added to
+         the job queue to be executed by the downloader.
+
+        """
         # queue of queue. Run each job in job_queue sequentially
         # run each job in job_queue in parallel
 
@@ -185,7 +239,19 @@ class Downloader:
         )
         self.job_queues.put(job_queue)
 
-    def run(self, num_threads: int = 10):
+    def run(self, num_threads: int = 10) -> None:
+        """
+        Executes queued download jobs in parallel using multiple threads.
+
+        This method starts the download process by executing the jobs queued in
+        `self.job_queues`. It processes each job queue sequentially, but runs the
+         jobs within each queue in parallel,up to the specified number of threads.
+         The next job_queue is executed if the previousjob_queue executes successfully
+
+        :param num_threads: The maximum number of worker threads to use for downloading
+         file chunks in parallel. Defaults to 10.
+
+        """
         is_prev_job_queue_success = True
         while not self.job_queues.empty() and is_prev_job_queue_success:
             job_queue = self.job_queues.get()
@@ -215,7 +281,20 @@ class Downloader:
                             future.result()
                 is_prev_job_queue_success = True
 
-    def execute_job_from_queue(self, job_queue: Queue[Job], **kwargs):
+    def execute_job_from_queue(self, job_queue: Queue[Job], **kwargs) -> None:
+        """
+        Executes a single job from the given job queue, managing retries and
+         synchronization.
+
+        Retrieves and executes the next job from the specified job queue, applying
+         any additional keyword arguments provided. Lock used to be thread safe
+         accessing next job from the queue. If the job execution fails, it will
+         retry the job based on  the job's retry policy until it succeeds or
+         exceeds the maximum number of retries.
+
+        :param job_queue: The queue from which to retrieve and execute a job.
+
+        """
         with self._lock:
             job: Job = job_queue.get()
         success = False
@@ -243,8 +322,26 @@ class Downloader:
         progress_bar: tqdm,
         headers: Dict[str, Any] = {},
         write_chunk_size: Optional[int] = None,
-    ):
-        write_chunk_size = min(500_000_000, self.file_size)
+    ) -> None:
+        """
+        Downloads a file or a chunk of a file to the specified path, updating a progress
+         bar as it downloads.
+
+        Downloads the file (or a chunk of it, if specified by the 'Range' header in
+         `headers`) from the URL associated with the downloader instance. It writes the
+         downloaded content to the file specified by `download_path`. The method updates
+         the given `progress_bar` object to reflect the progress of the download.
+
+        :param download_path: The path to which the file (or chunk) will be downloaded.
+        :param progress_bar: A tqdm progress bar instance to be updated as the file
+         downloads.
+        :param headers: Additional headers to include in the download request, such as
+            'Range' for chunked downloads. Defaults to an empty dictionary.
+        :param write_chunk_size: The size of chunks to write to the file at a time.
+         If not specified, a default size of 500MB is used
+
+        """
+        write_chunk_size = min(CHUNK_BYTES, self.file_size)
         create_parent_dirs(download_path)
         response = requests.get(
             self.url, headers=headers, stream=True, allow_redirects=True
@@ -255,7 +352,15 @@ class Downloader:
                     f.write(chunk)
                     progress_bar.update(len(chunk))
 
-    def combine_chunks_and_delete(self, download_path: str, progress_bar: tqdm):
+    def combine_chunks_and_delete(self, download_path: str, progress_bar: tqdm) -> None:
+        """
+        Combines the chunks and deletes the folder containing chunk files
+
+        :param download_path: path to combine the chunks, specified in object
+         instantiation
+        :param progress_bar: tqdm object showing the progress of combining chunks
+
+        """
         parent_directory = os.path.dirname(download_path)
         chunk_directory = os.path.join(parent_directory, "chunks")
 
@@ -278,6 +383,14 @@ class Downloader:
 
         shutil.rmtree(chunk_directory)
 
-    def get_chunk_file_path(self, file_range: str):
+    def get_chunk_file_path(self, file_range: str) -> str:
+        """
+        Generate chunk file path based on the download path specified in
+         object instantiation
+
+        :param file_range: additional string to extend to the file displaying the
+         range of chunk of the file
+
+        """
         parent_directory = os.path.dirname(self.download_path)
         return os.path.join(parent_directory, "chunks", file_range)
