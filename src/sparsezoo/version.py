5efced37ab47fd378cd81790418827ebab579562
Dipika Sikka <dipikasikka1@gmail.com>
2024-02-20 20:00:17 +0000

    update pr num formatting

--- src/sparsezoo/version.py
+++ src/sparsezoo/version.py
@@ -30,7 +30,7 @@ def _generate_version():
     if is_release:
         return version_base
     elif is_dev:
-        return f"{version_base}.dev[PR{dev_number}]"
+        return f"{version_base}.dev.PR{dev_number}"
     else:
         return f"{version_base}.{date.today().strftime('%Y%m%d')}"
 
